0aaa380ee6c895fcb6a787b4319d15be83ace400
Bam-Bam <141299964+Woohyeon-Hong@users.noreply.github.com>
2026-08-19 10:11:59 +0900

    Revert "[FEAT] 사용자 반응 검증을 위한 AI 활용 2차 MVP 구현"

--- src/main/java/com/wrb/devica/probe/event/ExperimentEventRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wrb.devica.probe.event;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExperimentEventRepository extends JpaRepository<ExperimentEvent, Long> {
-}
