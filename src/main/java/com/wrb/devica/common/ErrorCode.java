38de789e9772a15a222486680305ece0a3780875
kkb <gimgyubin1225@gmail.com>
2026-08-31 16:48:24 +0900

    fix: 존재하지 않는 경로 요청을 404로 응답
    favicon.ico 요청이나 경로 오타와 같은 클라이언트 요청 오류가 Exception 핸들러에 잡혀
    500 에러를 반환하고 ERROR 로그를 남기던 문제 수정

--- src/main/java/com/wrb/devica/common/ErrorCode.java
+++ src/main/java/com/wrb/devica/common/ErrorCode.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 경로를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "요청을 처리하지 못했습니다.");
 
     private final HttpStatus status;
