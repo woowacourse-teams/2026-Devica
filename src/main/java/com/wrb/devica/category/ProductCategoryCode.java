c41ab578aa9cbe6a8fc2f454668cae6721f75b37
kkb <gimgyubin1225@gmail.com>
2026-09-01 11:15:23 +0900

    feat: 제품 카테고리 코드로 enum 을 찾는 기능 추가
    사용자 요청에 포함된 code 값으로 enum 파싱

--- src/main/java/com/wrb/devica/category/ProductCategoryCode.java
+++ src/main/java/com/wrb/devica/category/ProductCategoryCode.java
@@ -1,6 +1,10 @@
 package com.wrb.devica.category;
 
+import com.wrb.devica.common.BusinessException;
+import com.wrb.devica.common.ErrorCode;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 @Getter
 public enum ProductCategoryCode {
@@ -11,5 +15,12 @@ public enum ProductCategoryCode {
 
     ProductCategoryCode(String displayName) {
         this.displayName = displayName;
+    }
+
+    public static ProductCategoryName from(String code) {
+        return Arrays.stream(values())
+                .filter(category -> category.name().equals(code))
+                .findFirst()
+                .orElseThrow(() -> new BusinessException(ErrorCode.PRODUCT_CATEGORY_NOT_FOUND));
     }
 }
