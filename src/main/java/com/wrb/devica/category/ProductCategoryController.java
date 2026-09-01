371ec0dca60fcdcbfefd473eb90d78bf75a47bc3
kkb <gimgyubin1225@gmail.com>
2026-09-01 10:56:44 +0900

    refactor: Controller가 ResponseEntity로 감싸서 응답하도록 변경
    응답별 상태코드 지정을 위해 ResponseEntity 래핑 방식으로 통일

--- src/main/java/com/wrb/devica/category/ProductCategoryController.java
+++ src/main/java/com/wrb/devica/category/ProductCategoryController.java
@@ -1,11 +1,11 @@
 package com.wrb.devica.category;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +15,10 @@ public class ProductCategoryController {
     private final ProductCategoryService productCategoryService;
 
     @GetMapping
-    public List<ProductCategoryResponse> findAll() {
-        return productCategoryService.findAll().stream()
-                .map(ProductCategoryResponse::from)
-                .toList();
+    public ResponseEntity<List<ProductCategoryResponse>> findAll() {
+        return ResponseEntity.ok()
+                .body(productCategoryService.findAll().stream()
+                        .map(ProductCategoryResponse::from)
+                        .toList());
     }
 }
