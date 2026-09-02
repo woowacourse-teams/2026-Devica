4ae299facdc73f60d22cf8a4adb7da3ef21894e4
kkb <gimgyubin1225@gmail.com>
2026-09-02 15:59:28 +0900

    feat: 노트북 권장 사양 데이터 추가
    사용 목적별 권장 사양을 LaptopRecommendation 상수로 관리한다.
    목적을 추가할 때 상수 하나만 더하면 되고 조회 코드는 바뀌지 않는다.

--- src/main/java/com/wrb/devica/common/BusinessErrorCode.java
+++ src/main/java/com/wrb/devica/common/BusinessErrorCode.java
@@ -12,7 +12,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum BusinessErrorCode implements ErrorCode {
 
-    PRODUCT_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 제품 종류입니다.");
+    PRODUCT_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 제품 종류입니다."),
+    USAGE_PURPOSE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용 목적입니다."),
+    RECOMMENDATION_NOT_FOUND(HttpStatus.NOT_FOUND, "권장 사양이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
