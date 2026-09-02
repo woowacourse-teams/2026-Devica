273a31902a04a1ddde893fdffea6ecb3d255ea68
woohyeon <woohyeon6885@gmail.com>
2026-09-02 13:13:36 +0900

    chore: LaptopRepository에서 불필요한 주석 삭제

--- src/main/java/com/wrb/devica/product/LaptopRepository.java
+++ src/main/java/com/wrb/devica/product/LaptopRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface LaptopRepository extends JpaRepository<Laptop, Long>, JpaSpecificationExecutor<Laptop> {
 
-    /**
-     * 전체 개수를 세지 않도록 slice 로 받는다. 정렬은 호출하는 쪽이 정한다.
-     */
     default Slice<Laptop> findAllByCondition(LaptopSearchCondition condition, Pageable pageable) {
-        return findBy(LaptopSpecification.of(condition), query -> query.slice(pageable));
+        return findBy(
+            LaptopSpecification.of(condition),
+            query -> query.slice(pageable)
+        );
     }
 }
