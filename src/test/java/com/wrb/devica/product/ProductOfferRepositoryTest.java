c5155cdaae6ccbb21bbb5924043974b6d2cc1d5a
woohyeon <woohyeon6885@gmail.com>
2026-09-03 15:08:41 +0900

    test: 픽스처를 빌더로 정리
    - 인자 위치를 세지 않도록 엔티티마다 빌더를 둔다. 지정하지 않은 값은 기본값을 쓴다
    - 검증하려는 값만 테스트에 남는다. 무관한 사양이 인자 자리를 채우지 않는다
    - 제품 코드를 이름 대신 시퀀스로 만든다. 같은 이름을 두 번 저장해도 유니크 제약을 어기지 않는다
    - 카테고리는 코드당 한 줄만 존재하므로 만들기 전에 찾는다
    - 오퍼 생성을 노트북 저장에서 분리한다. 오퍼가 없는 노트북이 코드에 그대로 드러난다
    - 중복되던 테스트를 합친다. 다음 페이지 여부와 판매 중이 아닌 오퍼 제외가 각각 하나가 된다

--- src/test/java/com/wrb/devica/product/ProductOfferRepositoryTest.java
+++ src/test/java/com/wrb/devica/product/ProductOfferRepositoryTest.java
@@ -14,10 +14,11 @@ class ProductOfferRepositoryTest extends LaptopJpaTestSupport {
     @Test
     void 판매_중인_오퍼를_조회하면_가격_오름차순으로_반환한다() {
         // given
-        Laptop laptop = saveLaptopWithoutOffer("노트북", Os.WINDOWS, 10000, 16, 512);
-        saveOffer(laptop, 2_990_000L, OfferStatus.ON_SALE);
-        saveOffer(laptop, 2_850_000L, OfferStatus.ON_SALE);
-        saveOffer(laptop, 2_910_000L, OfferStatus.ON_SALE);
+        Laptop laptop = laptop().name("노트북").save();
+
+        offer().product(laptop).price(2_990_000L).status(OfferStatus.ON_SALE).save();
+        offer().product(laptop).price(2_850_000L).status(OfferStatus.ON_SALE).save();
+        offer().product(laptop).price(2_910_000L).status(OfferStatus.ON_SALE).save();
 
         // when
         List<ProductOffer> found = findOnSaleOffers(laptop);
@@ -30,10 +31,11 @@ class ProductOfferRepositoryTest extends LaptopJpaTestSupport {
     @Test
     void 판매_중이_아닌_오퍼는_조회되지_않는다() {
         // given
-        Laptop laptop = saveLaptopWithoutOffer("노트북", Os.WINDOWS, 10000, 16, 512);
-        saveOffer(laptop, 900_000L, OfferStatus.SOLD_OUT);
-        saveOffer(laptop, 800_000L, OfferStatus.DISCONTINUED);
-        saveOffer(laptop, 2_000_000L, OfferStatus.ON_SALE);
+        Laptop laptop = laptop().name("노트북").save();
+
+        offer().product(laptop).price(900_000L).status(OfferStatus.SOLD_OUT).save();
+        offer().product(laptop).price(800_000L).status(OfferStatus.DISCONTINUED).save();
+        offer().product(laptop).price(2_000_000L).status(OfferStatus.ON_SALE).save();
 
         // when
         List<ProductOffer> found = findOnSaleOffers(laptop);
@@ -45,10 +47,11 @@ class ProductOfferRepositoryTest extends LaptopJpaTestSupport {
     @Test
     void 다른_제품의_오퍼는_조회되지_않는다() {
         // given
-        Laptop laptop = saveLaptopWithoutOffer("대상", Os.WINDOWS, 10000, 16, 512);
-        saveOffer(laptop, 1_000_000L, OfferStatus.ON_SALE);
-        Laptop other = saveLaptopWithoutOffer("다른것", Os.WINDOWS, 10000, 16, 512);
-        saveOffer(other, 500_000L, OfferStatus.ON_SALE);
+        Laptop laptop = laptop().name("대상").save();
+        offer().product(laptop).price(1_000_000L).status(OfferStatus.ON_SALE).save();
+
+        Laptop other = laptop().name("다른것").save();
+        offer().product(other).price(500_000L).status(OfferStatus.ON_SALE).save();
 
         // when
         List<ProductOffer> found = findOnSaleOffers(laptop);
@@ -60,8 +63,8 @@ class ProductOfferRepositoryTest extends LaptopJpaTestSupport {
     @Test
     void 판매_중인_오퍼가_없으면_빈_결과를_반환한다() {
         // given
-        Laptop laptop = saveLaptopWithoutOffer("노트북", Os.WINDOWS, 10000, 16, 512);
-        saveOffer(laptop, 900_000L, OfferStatus.SOLD_OUT);
+        Laptop laptop = laptop().name("노트북").save();
+        offer().product(laptop).price(900_000L).status(OfferStatus.SOLD_OUT).save();
 
         // when
         List<ProductOffer> found = findOnSaleOffers(laptop);
