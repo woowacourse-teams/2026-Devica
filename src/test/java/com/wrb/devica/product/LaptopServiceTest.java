72b71f551ddcfbe6cd39955765ea096db1c51a7f
woohyeon <woohyeon6885@gmail.com>
2026-09-09 23:10:24 +0900

    test: 슬라이스 테스트를 픽스처로 전환
    - 상속하던 지원 클래스를 지우고 픽스처와 FixtureSaver 를 주입받는다
    - 리포지토리 테스트 이름을 검증 대상인 LaptopRepositoryCustom 에 맞춘다
    - 카테고리와 cpu 를 테스트가 먼저 저장해 넘긴다. 저장 순서가 드러난다

--- src/test/java/com/wrb/devica/product/LaptopServiceTest.java
+++ src/test/java/com/wrb/devica/product/LaptopServiceTest.java
@@ -4,27 +4,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
 
+import com.wrb.devica.category.ProductCategory;
+import com.wrb.devica.category.ProductCategoryCode;
 import com.wrb.devica.common.BusinessErrorCode;
 import com.wrb.devica.common.BusinessException;
+import com.wrb.devica.common.JpaSliceTest;
+import com.wrb.devica.fixture.CpuFixture;
+import com.wrb.devica.fixture.FixtureSaver;
+import com.wrb.devica.fixture.LaptopFixture;
+import com.wrb.devica.fixture.ProductOfferFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Slice;
 
-@Import(LaptopService.class)
-class LaptopServiceTest extends LaptopJpaTestSupport {
+@JpaSliceTest
+@Import({LaptopService.class, FixtureSaver.class})
+class LaptopServiceTest {
 
     @Autowired
+    private FixtureSaver saver;
+
+    private ProductCategory category;
+
+    private Cpu cpu;
+    @Autowired
     private LaptopService laptopService;
+
+    @BeforeEach
+    void setUpCategory() {
+        category = saver.save(ProductCategory.from(ProductCategoryCode.LAPTOP));
+        cpu = saveCpu();
+    }
 
     @Test
     void 오퍼가_여러_개일_때_조회하면_가장_낮은_가격을_반환한다() {
         // given
-        Laptop laptop = laptop().name("노트북").save();
+        Laptop laptop = saver.save(LaptopFixture.laptop().category(category).cpu(cpu).name("노트북").build());
 
-        offer().product(laptop).price(2_500_000L).status(OfferStatus.ON_SALE).save();
-        offer().product(laptop).price(1_900_000L).status(OfferStatus.ON_SALE).save();
-        offer().product(laptop).price(2_100_000L).status(OfferStatus.ON_SALE).save();
+        saver.save(ProductOfferFixture.offer().product(laptop).price(2_500_000L).status(OfferStatus.ON_SALE).build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(1_900_000L).status(OfferStatus.ON_SALE).build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(2_100_000L).status(OfferStatus.ON_SALE).build());
 
         // when
         Slice<LaptopSummaryResponse> found = findLaptops();
@@ -38,11 +59,11 @@ class LaptopServiceTest extends LaptopJpaTestSupport {
     @Test
     void 판매_중이_아닌_오퍼가_더_쌀_때_조회하면_최저가에_포함하지_않는다() {
         // given
-        Laptop laptop = laptop().name("노트북").save();
+        Laptop laptop = saver.save(LaptopFixture.laptop().category(category).cpu(cpu).name("노트북").build());
 
-        offer().product(laptop).price(900_000L).status(OfferStatus.SOLD_OUT).save();
-        offer().product(laptop).price(800_000L).status(OfferStatus.DISCONTINUED).save();
-        offer().product(laptop).price(2_000_000L).status(OfferStatus.ON_SALE).save();
+        saver.save(ProductOfferFixture.offer().product(laptop).price(900_000L).status(OfferStatus.SOLD_OUT).build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(800_000L).status(OfferStatus.DISCONTINUED).build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(2_000_000L).status(OfferStatus.ON_SALE).build());
 
         // when
         Slice<LaptopSummaryResponse> found = findLaptops();
@@ -56,13 +77,13 @@ class LaptopServiceTest extends LaptopJpaTestSupport {
     @Test
     void 노트북이_여러_대일_때_조회하면_각자의_최저가를_반환한다() {
         // given
-        Laptop first = laptop().name("첫번째").save();
-        offer().product(first).price(1_000_000L).status(OfferStatus.ON_SALE).save();
-        offer().product(first).price(1_200_000L).status(OfferStatus.ON_SALE).save();
+        Laptop first = saver.save(LaptopFixture.laptop().category(category).cpu(cpu).name("첫번째").build());
+        saver.save(ProductOfferFixture.offer().product(first).price(1_000_000L).status(OfferStatus.ON_SALE).build());
+        saver.save(ProductOfferFixture.offer().product(first).price(1_200_000L).status(OfferStatus.ON_SALE).build());
 
-        Laptop second = laptop().name("두번째").memoryGb(32).storageGb(1024).save();
-        offer().product(second).price(3_000_000L).status(OfferStatus.ON_SALE).save();
-        offer().product(second).price(2_800_000L).status(OfferStatus.ON_SALE).save();
+        Laptop second = saver.save(LaptopFixture.laptop().category(category).cpu(cpu).name("두번째").memoryGb(32).storageGb(1024).build());
+        saver.save(ProductOfferFixture.offer().product(second).price(3_000_000L).status(OfferStatus.ON_SALE).build());
+        saver.save(ProductOfferFixture.offer().product(second).price(2_800_000L).status(OfferStatus.ON_SALE).build());
 
         // when
         Slice<LaptopSummaryResponse> found = findLaptops();
@@ -79,8 +100,8 @@ class LaptopServiceTest extends LaptopJpaTestSupport {
     @Test
     void 조회하면_노트북과_cpu_정보를_응답에_담는다() {
         // given
-        Laptop laptop = laptop().name("gram Pro 16").memoryGb(32).storageGb(1024).save();
-        offer().product(laptop).price(2_850_000L).status(OfferStatus.ON_SALE).save();
+        Laptop laptop = saver.save(LaptopFixture.laptop().category(category).cpu(cpu).name("gram Pro 16").memoryGb(32).storageGb(1024).build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(2_850_000L).status(OfferStatus.ON_SALE).build());
 
         // when
         LaptopSummaryResponse response = findLaptops().getContent().getFirst();
@@ -100,11 +121,11 @@ class LaptopServiceTest extends LaptopJpaTestSupport {
     @Test
     void 상세를_조회하면_판매처를_가격_오름차순으로_담는다() {
         // given
-        Laptop laptop = laptop().name("gram Pro 16").memoryGb(32).storageGb(1024).save();
+        Laptop laptop = saver.save(LaptopFixture.laptop().category(category).cpu(cpu).name("gram Pro 16").memoryGb(32).storageGb(1024).build());
 
-        offer().product(laptop).price(2_990_000L).status(OfferStatus.ON_SALE).save();
-        offer().product(laptop).price(2_850_000L).status(OfferStatus.ON_SALE).save();
-        offer().product(laptop).price(2_500_000L).status(OfferStatus.SOLD_OUT).save();
+        saver.save(ProductOfferFixture.offer().product(laptop).price(2_990_000L).status(OfferStatus.ON_SALE).build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(2_850_000L).status(OfferStatus.ON_SALE).build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(2_500_000L).status(OfferStatus.SOLD_OUT).build());
 
         // when
         LaptopDetailResponse found = findLaptopById(laptop.getId());
@@ -126,8 +147,8 @@ class LaptopServiceTest extends LaptopJpaTestSupport {
     @Test
     void 판매_중인_오퍼가_없는_노트북의_상세를_조회하면_예외가_발생한다() {
         // given
-        Laptop laptop = laptop().name("판매종료").save();
-        offer().product(laptop).price(900_000L).status(OfferStatus.DISCONTINUED).save();
+        Laptop laptop = saver.save(LaptopFixture.laptop().category(category).cpu(cpu).name("판매종료").build());
+        saver.save(ProductOfferFixture.offer().product(laptop).price(900_000L).status(OfferStatus.DISCONTINUED).build());
 
         // when & then
         assertThatThrownBy(() -> findLaptopById(laptop.getId()))
@@ -137,13 +158,17 @@ class LaptopServiceTest extends LaptopJpaTestSupport {
     }
 
     private LaptopDetailResponse findLaptopById(Long id) {
-        flushAndClear();
+        saver.flushAndClear();
         return laptopService.findLaptopById(id);
     }
 
     private Slice<LaptopSummaryResponse> findLaptops() {
-        flushAndClear();
+        saver.flushAndClear();
         return laptopService.findLaptops(
             new LaptopSearchCondition(null, null, null, null, null, null, null, null), 0, 10);
+    }
+
+    private Cpu saveCpu() {
+        return saver.save(CpuFixture.cpu().build());
     }
 }
