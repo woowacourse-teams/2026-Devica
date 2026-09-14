6eecdf1d5d36c9fbfcbbc94866caa0b602700df0
kkb <gimgyubin1225@gmail.com>
2026-09-14 15:05:55 +0900

    refactor: read-only 트랜잭션에 @Transactional(readOnly = true) 적용

--- src/main/java/com/wrb/devica/question/QuestionService.java
+++ src/main/java/com/wrb/devica/question/QuestionService.java
@@ -5,13 +5,15 @@ import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
-
+    
+    @Transactional(readOnly = true)
     public List<Question> findByPurposeCode(String purposeCode) {
         UsagePurposeCode purpose = UsagePurposeCode.from(purposeCode);
         return questionRepository.findAllWithOptionsByPurpose(purpose).stream()
