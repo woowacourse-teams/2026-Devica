819dbf80662822fdd0b4b73ec4431eeff7931fdd
bin1225 <gimgyubin1225@gmail.com>
2026-09-15 14:45:53 +0900

    Revert "[FEAT] 맞춤 질문 조회 API 구현"

--- src/main/java/com/wrb/devica/question/option/CurrentOs.java
+++
@@ -1,9 +0,0 @@
-package com.wrb.devica.question.option;
-
-import com.wrb.devica.question.OptionCode;
-
-public enum CurrentOs implements OptionCode {
-
-    MACOS,  // Mac
-    WINDOWS // Windows
-}
